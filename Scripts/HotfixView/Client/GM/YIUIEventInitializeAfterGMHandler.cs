05caaf5d05c940466e94b0afa656476f6a00a437
LiShengYang-yiyi <379338943@qq.com>
2026-01-06 16:45:17 +0800

    GM功能

--- Scripts/HotfixView/Client/GM/YIUIEventInitializeAfterGMHandler.cs
+++ Scripts/HotfixView/Client/GM/YIUIEventInitializeAfterGMHandler.cs
@@ -1,15 +1,26 @@
-﻿namespace ET.Client
+﻿using YIUIFramework;
+
+namespace ET.Client
 {
     [Event(SceneType.All)]
     public class YIUIEventInitializeAfterGMHandler : AEvent<Scene, YIUIEventInitializeAfter>
     {
         protected override async ETTask Run(Scene scene, YIUIEventInitializeAfter arg)
         {
-            //根据需求自行处理 在editor下自动打开  也可以根据各种外围配置 或者 GM等级打开
-            //if (Define.IsEditor) //这里默认都打开
+            //新增 可以通过配置关闭GM功能
+            if (YIUIConstHelper.Const.CloseGMCommand)
+            {
+                return;
+            }
+
+            //根据需求自行处理 在Editor下自动打开  也可以根据各种外围配置 或者 GM等级打开
+            //#if UNITY_EDITOR
+
             {
                 scene.AddComponent<GMCommandComponent>();
             }
+
+            //#endif
 
             await ETTask.CompletedTask;
         }
